ef3e6811a0fd88e060f8a2c1f430e1a5d0ab325c
genisul <choisulim@gmail.com>
2024-08-25 10:17:03 +0900

    [New] 기능

---
+++ src/components/main/InputTodo.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="main-todo">
+    <input type="checkbox" />
+    <input type="text" class="input-box" />
+    <input type="date" />
+    <img class="edit" src="/src/assets/img/edit.png" />
+    <img class="delete" src="/src/assets/img/delete.png" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.container {
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  gap: 20px;
+  background-color: white;
+  width: 472px;
+  height: 497px;
+  border-width: 1px;
+  border-style: solid;
+  padding: 40px;
+}
+
+.main-todo {
+  display: flex;
+  width: 100%;
+
+  justify-content: space-around;
+}
+
+.input-box {
+  box-shadow: 0 2px 5px 4px rgba(0, 0, 0, 0.1);
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  height: 30px;
+}
+
+.edit {
+  width: 21px;
+  height: 21px;
+}
+
+.delete {
+  width: 21px;
+  height: 21px;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,14 @@
 <template>
-  <MainLayout />
+  <MainLayout>
+    <InputTodo />
+    <MainFooter />
+  </MainLayout>
 </template>
 
 <script setup lang="ts">
 import MainLayout from '@/layouts/MainLayout.vue'
+import InputTodo from '@/components/main/InputTodo.vue'
+import MainFooter from '@/components/main/MainFooter.vue'
 </script>
 
 <style scoped></style>
